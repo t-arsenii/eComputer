e6092b087eefac985d6f8ea6882e4a6270104e2f
Ivan Tkach <s97079@pollub.edu.pl>
2023-11-09 15:42:52 +0100

    ComputerBranch commit

---
+++ src/main/java/com/example/eComputer/domain/ComputerEntity.java
@@ -0,0 +1,33 @@
+package com.example.eComputer.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Computers")
+
+public class ComputerEntity {
+
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+
+
+        private String Motherboard;
+        private String RAM;
+        private String Hard_drive;
+        private String CPU;
+
+
+
+
+}
