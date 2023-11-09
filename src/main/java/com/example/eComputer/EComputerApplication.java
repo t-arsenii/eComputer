4f50a8b56e9b0500f2edcebe06dc5ad150716fce
Arsenii <senyazp80@gmail.com>
2023-11-09 14:43:47 +0100

    first commit

---
+++ src/main/java/com/example/eComputer/EComputerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eComputer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComputerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eComputer/EComputerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eComputer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComputerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
