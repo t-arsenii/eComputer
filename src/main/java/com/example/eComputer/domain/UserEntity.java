7a012e85eba0c71e84796a69a83084c365b65b5c
Arsenii <senyazp80@gmail.com>
2023-11-09 15:21:16 +0100

    new userEntity fields

--- src/main/java/com/example/eComputer/domain/UserEntity.java
+++ src/main/java/com/example/eComputer/domain/UserEntity.java
@@ -1,4 +1,28 @@
 package com.example.eComputer.domain;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean enabled;
+    private boolean tokenExpired;
 }
