bcdbda87717cb750eba400bccdcb418d1603f174
Arsenii <senyazp80@gmail.com>
2023-11-09 15:57:12 +0100

    user controller user Entity user Repository user Service

--- src/main/java/com/example/eComputer/domain/UserEntity.java
+++ src/main/java/com/example/eComputer/domain/UserEntity.java
@@ -4,13 +4,12 @@ package com.example.eComputer.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
-@Getter
-@Setter
-@Builder
-@EqualsAndHashCode
+import java.util.Date;
+
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "Users")
@@ -18,11 +17,8 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    private String firstName;
-    private String lastName;
+    private String name;
     private String email;
     private String password;
-    private boolean enabled;
-    private boolean tokenExpired;
+    private Date birthday;
 }
